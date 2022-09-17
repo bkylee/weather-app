358724982a12d488621cf12fe3fe59345e5c9f20
bkylee <brian.ky.lee@outlook.com>
2022-09-16 21:23:45 -0400

    Created functions for dom

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-function getData (location){ 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c74387e34986c811c7cffc7d4d5db861`, {mode:'cors'})
-    .then((response)=>{
-        return response.json();
-    })
-    .catch((err) =>{
-        //prompt user to enter a location 
-    })
-};
+let location = 'toronto';
+let dataJson = null;
+let weatherOb = {};
 
-const weatherData = (location)=>{
-    let data = getData(location);
-    let main = data.weather.main;
-    let description = data.weather.description;
+// .then((response)=>{
+    //     return response.json();
+    // });
+    // .then((response)=>{
+    //     return weatherData(response);
+    // });
+    // .catch((err) =>{
+    //    console.log(err);
+    // });
+
+function weatherData(data){
+    let main = data.weather[0].main;
+    let description = data.weather[0].description;
     let currentTemp = data.main.temp;
     let humidity = data.main.humidity;
     let feelsLike = data.main.feels_like;
-
     return{
         main,
         description,
@@ -24,4 +26,64 @@ const weatherData = (location)=>{
         feelsLike
     };
 };
+const searchWrapper = document.createElement('div');
+searchWrapper.setAttribute('id','searchWrap');
+document.body.appendChild(searchWrapper);
+
+const search = document.createElement('button');
+search.setAttribute('type','button');
+search.setAttribute('id','search');
+search.textContent = "Search";
+searchWrapper.appendChild(search);
+
+
+const searchI = document.createElement('input');
+searchI.setAttribute('type','text');
+searchI.setAttribute('id','searchI');
+searchWrapper.appendChild(searchI);
+
+const main = document.createElement('div');
+main.setAttribute('id','wrapper');
+document.body.appendChild(main);
+
+search.addEventListener('click',()=>{
+    const input = document.querySelector('#searchI').value;
+    if (input.toUpperCase() === location){
+        return;
+    };
+    location = input;
+    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c74387e34986c811c7cffc7d4d5db861`;
+    fetch(URL, {mode:'cors'})
+    .then(response=>{
+        response.json()
+        .then(post=>{
+            dataJson = post;
+            weatherOb = weatherData(dataJson);
+        });
+    });
+});
+
+function clearMain(){
+    while(main.children.length>0){
+        main.removeChild(main.lastChild);
+    };
+};
+
+function showCity(){
+    const city = document.createElement('h1');
+    city.textContent = location.toUpperCase();
+}
+
+function showMain(){
+    const weatherMain = document.createElement('div');
+    weatherMain.setAttribute('id','main');
+    weatherMain.textContent = weatherOb.main;
+    main.appendChild(weatherMain);
+};
+
+
+
+
+
+
 
